aa3764737b6b6751d636a291f18231f9f3dabe4a
Soros Liu <soros.liu1029@gmail.com>
2021-10-11 17:57:06 +0800

    setup explore env

---
+++ explore/start-jupyter.sh
@@ -0,0 +1,7 @@
+#!/bin/env bash
+docker run \
+    -d \
+    -p 8888:8888 \
+    -v "${PWD}/..":/home/jovyan \
+    jupyter/minimal-notebook:latest \
+    start-notebook.sh --NotebookApp.password='sha1:34147a04de8e:28b0c1d0c034adf65f78074e69253c7f83e18144'
