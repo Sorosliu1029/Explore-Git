6de2684f743307d55ca8164f23b487dc06b3d7ca
Soros Liu <soros.liu1029@gmail.com>
2021-10-14 18:10:52 +0800

    add mock play

--- explore/start-jupyter.sh
+++ explore/start-jupyter.sh
@@ -3,5 +3,6 @@ docker run \
     -d \
     -p 8888:8888 \
     -v "${PWD}/..":/home/jovyan \
+    -v /tmp:/tmp \
     jupyter/minimal-notebook:latest \
     start-notebook.sh --NotebookApp.password='sha1:34147a04de8e:28b0c1d0c034adf65f78074e69253c7f83e18144'
